7663dedb4b04f3ae669e30d2bd9a3d794efd1467
japinol <joan@192.168.1.34>
2019-06-06 11:12:24 +0200

    Small fix.

--- snakes/__init__.py
+++ snakes/__init__.py
@@ -1,7 +1,7 @@
 '''Package snakes.'''
 __author__ = 'Joan A. Pinol  (japinol)'
 __all__ = ['actors', 'apples', 'bats', 'bullets', 'cartridges', 'colors',
-           'constants', 'debug_info', 'experience_points.py', 'help_info.py',
+           'constants', 'debug_info', 'experience_points', 'help_info',
            'levels', 'lib_graphics_jp', 'lib_jp', 'mines', 'recovery_potions',
            'resources', 'score_bars', 'scores', 'screens', 'settings',
            'snakes', 'snakes_game',
